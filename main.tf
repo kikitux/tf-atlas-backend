d4c9d0f0e75354c1aff138383553a264614709d7
Alvaro Miranda Aguilera <kikitux@gmail.com>
2018-10-24 15:22:53 +0200

    update main for remote state backend

--- main.tf
+++ main.tf
@@ -1,3 +1,9 @@
+terraform {
+  backend "atlas" {
+    name    = "kikitux/tf-atlas-backend"
+  }
+}
+
 resource "null_resource" "helloWorld" {
   provisioner "local-exec" {
     command = "echo hello world"
